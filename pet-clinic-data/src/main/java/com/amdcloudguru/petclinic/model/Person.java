fac4c50a4f4ec7f8e3784947fbf53eca59e6ce22
Arafat Mohammed <arafat@dais.com>
2021-02-21 13:29:22 -0600

    Adding Base Entity. Closes #10

--- pet-clinic-data/src/main/java/com/amdcloudguru/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/amdcloudguru/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.amdcloudguru.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/amdcloudguru/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/amdcloudguru/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.amdcloudguru.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
