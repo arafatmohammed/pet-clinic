1a628148a8234bd207c6fae74dd8a8e4a9558f5b
Arafat Mohammed <arafat@dais.com>
2021-02-21 19:02:41 -0600

    Refactored Service Interface to Common Base Interface. Closes #11

--- pet-clinic-data/src/main/java/com/amdcloudguru/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/amdcloudguru/petclinic/services/PetService.java
@@ -2,13 +2,7 @@ package com.amdcloudguru.petclinic.services;
 
 import com.amdcloudguru.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>  {
 
-public interface PetService {
 
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
